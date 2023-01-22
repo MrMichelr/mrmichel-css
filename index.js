f4a9b24b68390e0d2e3463caa24c9d1833e79450
MrMichelr <michel92.rodriguez@gmail.com>
2023-01-22 16:30:05 +1300

    .gitignore should now work

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 
 var minify = new CleanCSS().minify(['./style/style.css'])
 
-fs.writeFile('./style/style.min.css', content, err => {
+console.log (minify)
+
+fs.writeFile('./style/style.min.css', minify.styles, err => {
     if (err) {
       console.error(err);
     }
